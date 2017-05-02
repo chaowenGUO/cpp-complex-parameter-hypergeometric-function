3e9e65644e21d4feb5b89818c9ecce0467f8ab4d
chaowen guo <gcwenken.guo@ttu.edu>
2017-05-02 15:47:20 -0500

    Update hyp2f1.cpp

--- hyp2f1.cpp
+++ hyp2f1.cpp
@@ -1,58 +1,66 @@
-#include<boost/numeric/odeint.hpp>
+#include <boost/numeric/odeint.hpp>
 
-namespace{
+namespace {
 
-std::complex<double> hypser(std::complex<double> const &a, std::complex<double> const &b, std::complex<double> const &c, std::complex<double> const &z, std::complex<double> &series)
+std::array<std::complex<double>, 2> hypser(std::complex<double> const &A, std::complex<double> const &B,
+                                           std::complex<double> const &C, std::complex<double> const &Z)
 {
-  std::complex<double> deriv=0.0;
-  std::complex<double> fac=1.0;
-  std::complex<double> temp=fac;
-  std::complex<double> aa=a;
-  std::complex<double> bb=b;
-  std::complex<double> cc=c;
-  for (int n=1;n<=1000;n++)
+  std::complex<double> series, deriv, fac{1};
+  auto temp{fac}, a{A}, b{B}, c{C};
+  for (std::size_t n{1}; n <= 1000; ++n)
   {
-    fac *= (aa*bb)/cc;
+    fac *= a * b / c;
     deriv += fac;
-    fac *= (1.0/n)*z;
-    series=temp+fac;
-    if (series == temp) return deriv;
-    temp=series;
-    aa += 1.0;
-    bb += 1.0;
-    cc += 1.0;
+    fac *= 1. / n * Z;
+    series = temp + fac;
+    if (series == temp)
+      return {series, deriv};
+    temp = series;
+    a += 1;
+    b += 1;
+    c += 1;
   }
   throw("convergence failure in hypser");
 }
 
-}//namespace
+} // namespace
 
-std::complex<double> hypgeo(std::complex<double> const &a, std::complex<double> const &b,std::complex<double> const &c, std::complex<double> const &z)
+std::complex<double> hyp2f1(std::complex<double> const &A, std::complex<double> const &B,
+                            std::complex<double> const &C, std::complex<double> const &Z)
 {
-  std::complex<double> ans,dz,z0;
-  std::array<std::complex<double>,2>y;
-  if (std::norm(z) <= 0.25)
+  std::complex<double> z0;
+  if (std::norm(Z) <= 0.25)
+    return hypser(A, B, C, Z).front();
+  else if (std::real(Z) < 0)
+    z0 = -0.5;
+  else if (std::real(Z) <= 1)
+    z0 = 0.5;
+  else
   {
-    y.back()=hypser(a,b,c,z,ans);
-    return ans;
+    using namespace std::literals;
+    z0 = std::imag(Z) >= 0 ? 0.5i : -0.5i;
   }
-  else if (std::real(z) < 0) z0=-0.5;
-  else if (std::real(z) <= 1) z0=0.5;
-  else z0=std::complex<double>(0.0,std::imag(z) >= 0.0 ? 0.5 : -0.5);
-  dz=z-z0;
-  y.back()=hypser(a,b,c,z0,y.front());
-  boost::numeric::odeint::integrate_adaptive(boost::numeric::odeint::bulirsch_stoer<std::array<std::complex<double>,2>>{1e-14,1e-14},
-      [&](std::array<std::complex<double>,2> const&y,std::array<std::complex<double>,2>&dyds,double const s)
+  auto const Dz{Z - z0};
+  auto dependentVariable{hypser(A, B, C, z0)};
+  boost::numeric::odeint::integrate_adaptive(
+      boost::numeric::odeint::bulirsch_stoer<decltype(dependentVariable)>{1e-14, 1e-14},
+      [&](auto const &DependentVariable, auto &derivative, double const Variable)
       {
-	dyds.front()=y.back()*dz;
-	auto const z{z0+s*dz};
-	dyds.back()=(a*b*y.front()-(c-(a+b+1.)*z)*y.back())*dz/(z*(1.-z));
-      }, y ,0.,1.,0.1);
-  return y.front();
+        derivative.front() = DependentVariable.back() * Dz;
+        auto const z{z0 + Variable * Dz};
+        derivative.back() =
+            (A * B * DependentVariable.front() - (C - (A + B + 1.) * z) * DependentVariable.back()) *
+            Dz / (z * (1. - z));
+      },
+      dependentVariable, 0., 1., 0.1);
+  return dependentVariable.front();
 }
 
 int main()
 {
   using namespace std::literals;
-  std::cout<<hypgeo(1.+1i,0.75,10i,1.+5i)<<std::endl;
+  std::cout << hyp2f1(1. + 1i, 0.75, 10i, 1. + 5i) << std::endl;
 }
+
+//compile//g++ -std=c++1z -O2 -march=native
+//test//import mpmath; mpmath.hyp2f1(1+j, 0.75, 10j, 1+5j)
