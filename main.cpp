f645b67a17ffd0a78f228db92886f79d79291563
chaowen guo <chaowen.guo1@gmail.com>
2020-03-03 19:11:51 -0600

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -12,3 +12,6 @@ int main()
 //compile//clang++-libc++ -std=c++2a -O2 -march=native -fmodules-ts --precompile hyp2f1.cppm
          //clang++-libc++ -std=c++2a -O2 -march=native -fmodules-ts -fprebuilt-module-path=. hyp2f1.pcm main.cpp
 //test//import mpmath; mpmath.hyp2f1(1+1j, 0.75, 10j, 1+5j)
+
+//g++ -std=c++2a -O2 -march=native -ftree-vectorize -ffast-math
+//clang++-libc++ -std=c++2a -O2 -march=native -fno-math-errno
