28cd95281475d3f7870ca34fcd61f765c503a1f8
chaowen guo <chaowen.guo1@gmail.com>
2021-07-12 14:16:11 -0500

    Update hyp2f1.cpp

--- hyp2f1.cpp
+++ hyp2f1.cpp
@@ -56,28 +56,16 @@ export std::complex<double> hyp2f1(std::complex<double> const &A, std::complex<d
 
 #include <boost/numeric/odeint.hpp>
 
-
-
-//[ rhs_function
-/* The type of container used to hold the state vector */
 typedef std::vector< double > state_type;
 
 const double gam = 0.15;
 
-/* The rhs of x' = f(x) */
-void harmonic_oscillator( const state_type &x , state_type &dxdt , const double /* t */ )
+void harmonic_oscillator( const state_type &x , state_type &dxdt , const double )
 {
     dxdt[0] = x[1];
     dxdt[1] = -x[0] - gam*x[1];
 }
-//]
 
-
-
-
-
-//[ rhs_class
-/* The rhs of x' = f(x) defined as a class */
 class harm_osc {
 
     double m_gam;
@@ -91,13 +79,7 @@ public:
         dxdt[1] = -x[0] - m_gam*x[1];
     }
 };
-//]
 
-
-
-
-
-//[ integrate_observer
 struct push_back_state_and_time
 {
     std::vector< state_type >& m_states;
@@ -112,18 +94,8 @@ struct push_back_state_and_time
         m_times.push_back( t );
     }
 };
-//]
 
-struct write_state
-{
-    void operator()( const state_type &x ) const
-    {
-        std::cout << x[0] << "\t" << x[1] << "\n";
-    }
-};
-
-
-int main(int /* argc */ , char** /* argv */ )
+int main()
 {
     using namespace std;
     using namespace boost::numeric::odeint;
