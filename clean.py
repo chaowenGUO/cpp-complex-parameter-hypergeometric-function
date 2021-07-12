1eae968247d592203b1361e0dd4e76f7af0e8cb8
chaowen guo <chaowen.guo1@gmail.com>
2021-07-12 18:59:03 -0500

    Update clean.py

--- clean.py
+++ clean.py
@@ -1,1 +1,11 @@
+import aiohttp, asyncio, argparse, os
+parser = argparse.ArgumentParser()
+parser.add_argument('github')
 
+async def main():
+    async with aiohttp.ClientSession() as session:
+        async with session.get(f'https://api.github.com/repos/{os.getenv("GITHUB_REPOSITORY")}/actions/runs?per_page=100&page=1') as response:
+            for _ in (await response.json()).get('workflow_runs'):
+                async with session.delete(f'https://api.github.com/repos/{os.getenv("GITHUB_REPOSITORY")}/actions/runs/{_.get("id")}', auth=aiohttp.BasicAuth('chaowenguo', parser.parse_args().github)) as _: pass
+
+asyncio.run(main())
