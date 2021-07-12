6cb612d103ece72787049d51f8f30b1621eef46b
chaowen guo <chaowen.guo1@gmail.com>
2021-07-12 13:43:36 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -7,7 +7,7 @@ int main()
 {
     using namespace std::literals;
     std::cout << hyp2f1(1. + 1i, 0.75, 10i, 1. + 5i) << std::endl;
-}*/
+}
 
 //compile//clang++-libc++ -std=c++2a -O2 -march=native -fmodules-ts --precompile hyp2f1.cppm
          //clang++-libc++ -std=c++2a -O2 -march=native -fmodules-ts -fprebuilt-module-path=. hyp2f1.pcm main.cpp
