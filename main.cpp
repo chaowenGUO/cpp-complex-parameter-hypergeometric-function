2e9f007a6081ff9e46913d7ea1ed96e6bfbd2a29
chaowen guo <chaowen.guo1@gmail.com>
2021-07-12 15:15:19 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
-#include "hyp2f1.h"
 #include<iostream>
 #include<complex>
+
+std::complex<double> hyp2f1(std::complex<double> const &A, std::complex<double> const &B, std::complex<double> const &C, std::complex<double> const &Z);
 
 int main()
 {
