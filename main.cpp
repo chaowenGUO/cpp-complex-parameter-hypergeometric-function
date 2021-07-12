c38417fc59ae25c03b8d7f845ac53cea3f8435f3
chaowen guo <chaowen.guo1@gmail.com>
2021-07-12 16:55:34 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,7 +1,7 @@
+import hyp2f1;
+
 #include<iostream>
 #include<complex>
-
-std::complex<double> hyp2f1(std::complex<double> const &A, std::complex<double> const &B, std::complex<double> const &C, std::complex<double> const &Z);
 
 int main()
 {
