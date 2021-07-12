09beba9f3cf734e34ed80fe08c92dc8377edf1ad
chaowen guo <chaowen.guo1@gmail.com>
2021-07-12 13:30:47 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,4 +1,4 @@
-#include<iostream>
+/*#include<iostream>
 #include<complex>
 
 import hyp2f1;
@@ -7,7 +7,7 @@ int main()
 {
     using namespace std::literals;
     std::cout << hyp2f1(1. + 1i, 0.75, 10i, 1. + 5i) << std::endl;
-}
+}*/
 
 //compile//clang++-libc++ -std=c++2a -O2 -march=native -fmodules-ts --precompile hyp2f1.cppm
          //clang++-libc++ -std=c++2a -O2 -march=native -fmodules-ts -fprebuilt-module-path=. hyp2f1.pcm main.cpp
@@ -15,3 +15,9 @@ int main()
 
 //g++ -std=c++2a -O2 -march=native -ftree-vectorize -ffast-math
 //clang++-libc++ -std=c++2a -O2 -march=native -fno-math-errno
+
+import hyp2f1;
+ 
+int main() {
+    hello();
+}
